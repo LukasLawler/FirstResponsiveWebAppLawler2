6dab85c4124bc0564de57f5ea3b8132c41dc2393
Luke Lawler <headfirstluke377@live.com>
2021-01-29 21:21:37 -0600

    e

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstResponsiveWebAppLawler.Models;
+
+namespace FirstResponsiveWebAppLawler.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Age = 0;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Person person, string dummy)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Age = person.AgeThisYear();
+            }
+            else
+            {
+                ViewBag.Age = 0;
+            }
+            return View(person);
+        }
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace FirstResponsiveWebAppLawler.Models
+{
+
+    public class Person
+    {
+        [Required(ErrorMessage = "Must input name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Must input birth year")]
+        [Range(1900, 2021, ErrorMessage = "Birth year must be between 1900 and 2021.")]
+        public int? BirthYear { get; set; }
+        
+        public int? AgeThisYear()
+        {
+            int CURRENTYEAR = 2021;
+            int? currentAge = CURRENTYEAR - BirthYear;
+            return currentAge;
+        }
+    }
+}
